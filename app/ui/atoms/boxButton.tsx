8c3f510df386a01adae5d4bdd7a974a5ed42f2e2
SEMIN-97 <jsemin1021@gmail.com>
2024-04-17 23:44:32 +0900

    Feat: BoxButton component 생성
    * story 작성

---
+++ app/ui/atoms/boxButton.tsx
@@ -0,0 +1,47 @@
+import clsx from 'clsx';
+
+import styles from './boxButton.module.scss';
+
+interface BoxButtonProps {
+  label: string;
+  disabled?: boolean;
+  icon?: string;
+  iconPos?: 'left' | 'right';
+  onClick?: () => void;
+  rounded?: boolean;
+  size?: 'small' | 'medium' | 'large';
+  variant?: 'primary' | 'secondary' | 'plain';
+}
+
+const BoxButton = ({
+  disabled = false,
+  rounded = false,
+  iconPos = 'left',
+  size = 'medium',
+  variant = 'primary',
+  icon,
+  label,
+  onClick
+}: BoxButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={clsx(
+        styles.boxButton,
+        styles[size],
+        styles[variant],
+        {
+          [styles.rounded]: rounded,
+          [styles.iconRight]: 'right' === iconPos,
+        }
+      )}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      { icon && <i className="material-symbols-rounded">{ icon }</i> }
+      { label }
+    </button>
+  );
+};
+
+export default BoxButton;
---
+++ stories/boxButton.stories.ts
@@ -0,0 +1,80 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import BoxButton from '@/app/ui/atoms/boxButton';
+
+const meta = {
+  title: 'Atoms/BoxButton',
+  component: BoxButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    icon: {
+      description: '사용할 Google Icon을 입력해주세요 <br>예) Search'
+    },
+    iconPos: {
+      description: 'Icon에 값이 있는 경우에만 유효합니다'
+    }
+  },
+  args: {
+    label: '검색',
+    variant: 'primary',
+    size: 'medium',
+    onClick: fn()
+  },
+} satisfies Meta<typeof BoxButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = { };
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary'
+  }
+};
+
+export const Plain: Story = {
+  args: {
+    variant: 'plain'
+  }
+};
+
+export const Icon: Story = {
+  args: {
+    icon: 'Search'
+  }
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large'
+  }
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium'
+  }
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small'
+  }
+};
+
+export const Rounded: Story = {
+  args: {
+    rounded: true
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true
+  }
+};
